4d5e9ef519a4d3e0c69e806dfa627313f40aa325
dalpaka <120773637+dalpaka@users.noreply.github.com>
2023-11-01 07:36:45 +0000

    work on clrflow.js

---
+++ clrflow.js/index.js
@@ -0,0 +1,12 @@
+console.log('\x1b[36m Hi, devs!👋 \x1b[0m');
+
+
+
+
+
+
+
+
+function clrflow(value, foreground, background){
+    console.log(`\x1b[\x1b[`)
+}
---
+++ clrflow.js/main.js
@@ -0,0 +1,1 @@
+console.log('\x1b[34m Hi, devs!👋 \x1b[0m');
